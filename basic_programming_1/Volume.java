460be0355f640a5f291bbfc4307ae2ff50a33682
anshikaagrawal602-hue <anshikaagrawal602@gmail.com>
2026-01-21 15:15:58 +0530

    [Anshika Agrawal] has added basic_programming_1

---
+++ basic_programming_1/Volume.java
@@ -0,0 +1,9 @@
+class Volume {
+    public static void main(String[] args) {
+        int r = 2;
+        int h = 5;
+        double pi = 3.14;
+        double vol = pi*r*r*h;
+        System.out.println("vol of cylinder is:" + vol);
+    }
+}
---
+++ basic_programming_1/miles.java
@@ -0,0 +1,7 @@
+class miles{
+	public static void main(String args[]){
+		int km=3;
+		double m= km*0.621371;
+		System.out.println("Kilometer to miles is: "+ m);
+	}
+}
