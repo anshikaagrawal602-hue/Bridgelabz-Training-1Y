b0a75f20c07b85c1e695ec10f212f2831c3fdf6a
anshikaagrawal602-hue <anshikaagrawal602@gmail.com>
2026-01-26 16:50:35 +0530

    [Anshika Agrawal]has add basic programming 1 in programming constructs

---
+++ basic_programming_1/avg.java
@@ -0,0 +1,9 @@
+class avg {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 15;
+        double avg = (a+b+c)/3;
+        System.out.println("avg is:" + avg);
+    }
+}
